9721878dce0d4e94eb620eb765ba83d424f33b00
MVDLNE <madelinelora95@icloud.com>
2025-07-02 23:45:34 -0400

    added fav controller

---
+++ src/main/java/com/example/FavTracker/FavTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FavTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/FavTracker/controllers/FavsController.java
@@ -0,0 +1,4 @@
+package com.example.FavTracker.controllers;
+
+public class FavsController{
+}
