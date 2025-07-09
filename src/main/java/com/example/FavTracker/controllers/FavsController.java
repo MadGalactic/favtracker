b3d8383749b9b2108c3aedeb9b60f414050950c8
MVDLNE <madelinelora95@icloud.com>
2025-07-08 23:50:32 -0400

    enhancing form options

--- src/main/java/com/example/FavTracker/controllers/FavsController.java
+++ src/main/java/com/example/FavTracker/controllers/FavsController.java
@@ -14,13 +14,11 @@ public class FavsController{
         return "<html>" +
                 "<body>" +
                 "<h1>Welcome to the Favorite's Tracker!</h1>" +
-                "<h2>Here you can track all of your favorite colors. You can choose from the options below:</h2>" +
+                "<h2>Here you can track all of your favorite of your favorite things. You can choose from the options below:</h2>" +
                 "<ol>" +
-                "<li>Fuchsia</li>" +
-                "<li>Onyx</li>" +
-                "<li>Indigo</li>" +
-                "<li>Scarlet</li>" +
-                "<li>Yellow</li>" +
+                "<li>Your Favorite Color</li>" +
+                "<li>Your Favorite Food</li>" +
+                "<li>Your Favorite Hobby</li>" +
                 "</ol>";
 
     }
@@ -30,26 +28,45 @@ public class FavsController{
         return "<html>" +
                 "<body>" +
                 "<form action = '/form' method = 'post'>" + // submit a request to /hello
+                "<h2>Name:</h2>" +
                 "<input type='text' name='name' placeholder='Enter Your Name'>" +
+                "<p>My Favorite Color:</p>" +
                 "<select name='color' id='color'>" +
                 "<option value='fuchsia'>fuchsia</option>" +
                 "<option value='onyx'>onyx</option>" +
                 "<option value='indigo'>indigo</option>" +
                 "<option value='scarlet'>scarlet</option>" +
                 "<option value='yellow'>yellow</option>" +
-                "<input type='submit' value='Enter'>" +
                 "</select>" +
-                "</form>" +
+                "<p>My Favorite Food:</p>" +
+                "<select name='food' id='food'>" +
+                "<option value='pasta'>pasta</option>" +
+                "<option value='sushi'>sushi</option>" +
+                "<option value='tacos'>tacos</option>" +
+                "<option value='dumplings'>dumplings</option>" +
+                "<option value='salads'>salads</option>" +
+                "</select>" +
+                "<p>My Favorite Hobby:</p>" +
+                "<select name='sports' id='sports'>" +
+                "<option value='music'>music</option>" +
+                "<option value='dancing'>dancing</option>" +
+                "<option value='drawing'>drawing</option>" +
+                "<option value='skateboarding'>skateboarding</option>" +
+                "<option value='cooking'>cooking</option>" +
+                "</select>" +
+                "<br>" +
+                "<input type='submit' value='Enter'>" +
+                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
     @PostMapping("/form")
-    public String greetUser(@RequestParam String name, @RequestParam String color) {
+    public String greetUser(@RequestParam String name, @RequestParam String color, @RequestParam String food, @RequestParam String hobby) {
         if (name == null) {
             name = "World";
         }
-        return createMessage(name, color);
+        return createMessage(name, color, food, hobby);
     }
 
     public static String createMessage(String n, String l) {
@@ -68,5 +85,7 @@ public class FavsController{
         }
         return n +"'s favorite color is " + color ;
     }
+
+    // TODO: finish building conditionals
 
 }
