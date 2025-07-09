99e6d35c18a275a8124480dd1dacb707859523c9
MVDLNE <madelinelora95@icloud.com>
2025-07-08 23:10:39 -0400

    building form

--- src/main/java/com/example/FavTracker/controllers/FavsController.java
+++ src/main/java/com/example/FavTracker/controllers/FavsController.java
@@ -18,9 +18,9 @@ public class FavsController{
                 "<h1>Welcome to the Favorite's Tracker!</h1>" +
                 "<h2>Here you can track all of your favorite colors. You can choose from the options below:</h2>" +
                 "<ol>" +
-                "<li>Pink</li>" +
-                "<li>Black</li>" +
-                "<li>Blue</li>" +
+                "<li>Fuchsia</li>" +
+                "<li>Onyx</li>" +
+                "<li>Indigo</li>" +
                 "<li>Scarlet</li>" +
                 "<li>Yellow</li>" +
                 "</ol>";
@@ -32,4 +32,22 @@ public class FavsController{
     //  second favorite, and third favorite programming languages on your list.
     //  Use select elements for each of the rankings.
 
+    @GetMapping("/form")
+    public String favForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action = '/form' method = 'post'>" + // submit a request to /hello
+                "<input type='text' name='name'>" +
+                "<select name='color' id='color'>" +
+                "<option value='fuchsia'>fuchsia</option>" +
+                "<option value='onyx'>onyx</option>" +
+                "<option value='indigo'>indigo</option>" +
+                "<option value='scarlet'>scarlet</option>" +
+                "<option value='yellow'>yellow</option>" +
+                "<input type='submit' value='Thanks!'>" +
+                "</select>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
