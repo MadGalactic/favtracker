462d94408ecd21c5a2d979d6666dbc0b306bb248
MVDLNE <madelinelora95@icloud.com>
2025-07-08 22:56:24 -0400

    building controller

--- src/main/java/com/example/FavTracker/controllers/FavsController.java
+++ src/main/java/com/example/FavTracker/controllers/FavsController.java
@@ -1,4 +1,19 @@
 package com.example.FavTracker.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping
 public class FavsController{
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
 }
