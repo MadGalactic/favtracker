444b84dc85c9afcc3799cf304fde2f9e2db0c5b5
MVDLNE <madelinelora95@icloud.com>
2025-07-08 23:03:03 -0400

    added intro headings and copy

--- src/main/java/com/example/FavTracker/controllers/FavsController.java
+++ src/main/java/com/example/FavTracker/controllers/FavsController.java
@@ -13,7 +13,23 @@ public class FavsController{
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "<html>" +
+                "<body>" +
+                "<h1>Welcome to the Favorite's Tracker!</h1>" +
+                "<h2>Here you can track all of your favorite colors. You can choose from the options below:</h2>" +
+                "<ol>" +
+                "<li>Pink</li>" +
+                "<li>Black</li>" +
+                "<li>Blue</li>" +
+                "<li>Scarlet</li>" +
+                "<li>Yellow</li>" +
+                "</ol>";
+
     }
+
+    // TODO: At localhost:8080/form,
+    //  add a form that lets the user enter their name and choose their favorite,
+    //  second favorite, and third favorite programming languages on your list.
+    //  Use select elements for each of the rankings.
 
 }
